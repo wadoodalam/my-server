073d57ddc06edc7feb4bc9eae8bbad3e62a2fffc
wadoodalam <wadoodalam@outlook.com>
2023-11-19 18:34:16 -0800

    Tables created in ddb

---
+++ dynamo_model.js
@@ -0,0 +1,74 @@
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+    region: 'us-east-2', 
+    accessKeyId: 'AKIAXNVZZ3TH6IE7ENIQ',
+    secretAccessKey: '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',
+});
+
+
+const dynamodb = new AWS.DynamoDB();
+
+
+
+// Used to create tables
+const createTable = (tableName, keySchema, attributeDefinitions) => {
+    const params = {
+        TableName: tableName,
+        KeySchema: keySchema,
+        AttributeDefinitions: attributeDefinitions,
+        ProvisionedThroughput: {
+            ReadCapacityUnits: 5,
+            WriteCapacityUnits: 5,
+        },
+    };
+
+    dynamodb.createTable(params, (err, data) => {
+        if (err) {
+            if (err.code === 'ResourceInUseException') {
+                console.log(`Table ${tableName} already exists.`);
+            } else {
+                console.error(`Error creating table ${tableName}:`, err);
+            }
+        } else {
+            console.log('Created table:', data);
+        }
+    });
+};
+
+// Create Users table
+createTable(
+    'Users',
+    [
+        { AttributeName: 'user_id', KeyType: 'HASH' }, // Primary key: user_id
+    ],
+    [
+        { AttributeName: 'user_id', AttributeType: 'S' }, // Attribute definition 
+
+    ]
+);
+
+// Create Trips table
+createTable(
+    'Trips',
+    [
+        { AttributeName: 'trip_id', KeyType: 'HASH' }, // Primary key: trip_id
+    ],
+    [
+        { AttributeName: 'trip_id', AttributeType: 'S' }, // Attribute definition 
+  
+    ]
+);
+
+// Create UserTrips table (association table)
+createTable(
+    'UserTrips',
+    [
+        { AttributeName: 'userTrips_id', KeyType: 'HASH' }, // Primary key: user_id
+     
+    ],
+    [
+        { AttributeName: 'userTrips_id', AttributeType: 'S' }, // Attribute definition 
+        
+    ]
+);
